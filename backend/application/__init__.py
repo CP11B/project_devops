8d73429e90784cf64d8e588934ed367bbfe36508
Ubuntu <ubuntu@ip-172-31-23-222.eu-west-2.compute.internal>
2021-04-23 09:04:47 +0000

    adding changes to DB

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -4,9 +4,9 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = str(getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:R00tP4ssw0rd!@testdb.cyfbo9c0392o.eu-west-2.rds.amazonaws.com:3306/users'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = str(getenv('SECRET_KEY'))
+app.config['SECRET_KEY'] = str('auiruabgub')
 db = SQLAlchemy(app)
 
 from application import routes
